5ab79c3734081a7684ca30608aba7ec76bf631be
Seony777 <addiolsw1@gmail.com>
2026-01-08 12:14:12 +0900

    feat: Footer 컴포넌트 구현

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
-import React from "react";
-
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="flex justify-center items-center gap-[72px] self-stretch text-600 font-sans text-[20px] leading-[150%] tracking-[-0.4px]">
+      <p>앱에 대한 문의: UMC@example.com</p>
+      <p>개인정보 보호정책</p>
+      <p>이용 약관</p>
+    </div>
+  );
 };
 
 export default Footer;
